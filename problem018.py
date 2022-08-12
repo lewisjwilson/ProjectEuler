e039187212479a3a9c0fafaf97ad19623d48d8cf
Thicksandwich Development <lewis.j.wilson@outlook.com>
2022-08-12 15:06:49 +0900

    Revert "Update problem018.py"
    This reverts commit 08f076dbbb4386977611d32bec1ffa446cfbd5e8.

--- problem018.py
+++ problem018.py
@@ -1,4 +1,4 @@
-# A* Algorithm - https://medium.com/@nicholas.w.swift/easy-a-star-pathfinding-7e6689c7f7b2
+# A* Algorithm
 
 tri = [[75],
 [95,64],
@@ -16,39 +16,16 @@ tri = [[75],
 [63,66,4,68,89,53,67,30,73,16,69,87,40,31],
 [4,62,98,27,23,9,70,98,73,93,38,53,60,4,23]]
 
-def check_fscore(open_nodes):
-    highest_cost = 0
-    value = 0
-    idx = 0
-    for item in open_nodes:
-        g = 0 + item
-        h = len(tri) - item
-        f = g + h
-        if f > highest_cost:
-            highest_cost = f
-            value = item
-        idx += 1
-    return [value, highest_cost]
-    
 max_val = 0 # maximum ESTIMATED distance between first and end node (most likely higher)
 for x in range(len(tri)):
     max_val += max(tri[x])
-print("Maximum Possible Value:", max_val)
+print(max_val)
 
 f = 0 # total cost of the node (f = h + g)
 g = 0 # distance between start node and current node
 h = 0 # heuristic (estimated distance from current to end node)
 
-open_nodes = []
-closed_nodes = []
-open_nodes.append(tri[0][0])
+nodes = []
+nodes.append(tri[0][0])
 
-[value, fscore] = check_fscore(open_nodes)
-print("Open:",open_nodes,"Closed:",closed_nodes)
-closed_nodes.append(value)
-open_nodes.pop(open_nodes.index(value))
-
-print("Value in open_nodes:",value)
-print("F-score:", fscore)
-
-print("Open:",open_nodes,"Closed:",closed_nodes)
+print(nodes)
