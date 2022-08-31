fa639e1a8468d8ae3ca6a75dd164d3dfac4708e5
Lewis Wilson <55784291+lewisjwilson@users.noreply.github.com>
2022-08-31 09:12:19 +0900

    problem 19 complete

--- problem019.java
+++ problem019.java
@@ -1,7 +1,20 @@
+// Question
+// You are given the following information, but you may prefer to do some research for yourself.
+// 1 Jan 1900 was a Monday.
+// Thirty days has September,
+// April, June and November.
+// All the rest have thirty-one,
+// Saving February alone,
+// Which has twenty-eight, rain or shine.
+// And on leap years, twenty-nine.
+// A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
+// How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+
+
+// Solution
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
 
 public class problem019 {
 
@@ -12,43 +25,22 @@ public class problem019 {
         LocalDate startDate = LocalDate.parse(start);
         LocalDate endDate = LocalDate.parse(end);
 
-        System.out.println("Start Date : " + startDate);
-        System.out.println("This date was a: " + startDate.getDayOfWeek());
-        System.out.println("End Date : " + endDate);
-        System.out.println("This date was a: " + endDate.getDayOfWeek());
+        String[] months = {"01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"};
 
-        // If not a leap year, the following year, same date is the next weekday
-        // Leap year pushes the days forward by one
-
-        // 31 days in January. In year 1901 (not a leap year), staring on a tuesday
-        // If 1st, 2nd, 3rd, 4th are a Sunday, there are 5 Sundays in the Month, else 4
-
-        int janSundays = 0;
+        int sundays = 0;
         // For each year between the start and end dates
         for (int y = startDate.getYear(); y <= endDate.getYear(); y++) {
-            System.out.println(y);
-            LocalDate first = LocalDate.parse(String.valueOf(y) + "-01-01");
-            LocalDate second = LocalDate.parse(String.valueOf(y) + "-01-02");
-            LocalDate third = LocalDate.parse(String.valueOf(y) + "-01-03");
-            LocalDate fourth = LocalDate.parse(String.valueOf(y) + "-01-04");
-            System.out.println(first.getDayOfWeek() + " " + second.getDayOfWeek() + " " + third.getDayOfWeek() + " "
-                    + fourth.getDayOfWeek());
-            boolean sundayFound = false;
-            if (first.getDayOfWeek() == DayOfWeek.SUNDAY ||
-                    second.getDayOfWeek() == DayOfWeek.SUNDAY ||
-                    third.getDayOfWeek() == DayOfWeek.SUNDAY ||
-                    fourth.getDayOfWeek() == DayOfWeek.SUNDAY) {
-                sundayFound = true;
+            //System.out.println(y);
+            for(int m = 0 ; m < 12 ; m++){
+                LocalDate first = LocalDate.parse(String.valueOf(y) + "-" + months[m] + "-01");
+                boolean sundayFound = false;
+                if (first.getDayOfWeek() == DayOfWeek.SUNDAY){
+                    sundays++;
+                }
+                
             }
-
-            if (sundayFound) {
-                janSundays += 5;
-            } else {
-                janSundays += 4;
-            }
-
-            System.out.println("January Sundays: " + janSundays);
+            
         }
-
+        System.out.println("Sundays: " + sundays);
     }
 }
