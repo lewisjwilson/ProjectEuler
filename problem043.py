72c09ae0c98d6da1506c656183b5799166f137f2
Lewis Wilson <55784291+lewisjwilson@users.noreply.github.com>
2023-02-01 12:29:00 +0900

    Refactoring for readability

--- problem043.py
+++ problem043.py
@@ -15,16 +15,35 @@
 # Solution
 
 from itertools import permutations 
-  
+
+def check(d):
+    if((int(d[0]+d[1]+d[2])%2!=0)):
+        return False
+    if((int(d[2]+d[3]+d[4])%3!=0)):
+        return False
+    if((int(d[3]+d[4]+d[5])%5!=0)):
+        return False
+    if((int(d[4]+d[5]+d[6])%7!=0)):
+        return False
+    if((int(d[5]+d[6]+d[7])%11!=0)):
+        return False
+    if((int(d[6]+d[7]+d[8])%13!=0)):
+        return False
+    if((int(d[7]+d[8]+d[9])%17!=0)):
+        return False
+    return True
+    
 perm = permutations([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
 
 valid = list()
 
 for p in perm:
-    [d1, d2, d3, d4, d5, d6, d7, d8, d9, d10] = [str(p[0]), str(p[1]), str(p[2]), str(p[3]), str(p[4]), str(p[5]), str(p[6]), str(p[7]), str(p[8]), str(p[9])]
     
-    if((int(d2+d3+d4)%2==0) and (int(d3+d4+d5)%3==0) and (int(d4+d5+d6)%5==0) and (int(d5+d6+d7)%7==0) and (int(d6+d7+d8)%11==0) and (int(d7+d8+d9)%13==0) and (int(d8+d9+d10)%17==0)):
-        n = int(d1+d2+d3+d4+d5+d6+d7+d8+d9+d10)
+    d = list(p)
+    d = [str(x) for x in d]
+    
+    if(check(d)):
+        n = int(d[0]+d[1]+d[2]+d[3]+d[4]+d[5]+d[6]+d[7]+d[8]+d[9])
         valid.append(n)
         print(f"{n} is such a pandigital number!")
 
