4b8a47e615b74ae8c4cb2cf5504199833910b1e8
Lewis Wilson <55784291+lewisjwilson@users.noreply.github.com>
2023-03-02 14:14:04 +0900

    Create problem041.java

---
+++ problem041.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.math.BigInteger;
+
+class LargestPandigitalPrime {
+    
+    public static boolean allDigits(int i){
+    
+        String iStr = Integer.toString(i);
+        int l = iStr.length();
+        
+        ArrayList<Integer> iArrList = new ArrayList<Integer>();
+        
+        for(int x=0; x<l; x++){
+            int digit = Integer.valueOf(iStr.substring(x, x+1));
+            iArrList.add(digit);
+        }
+        
+        ArrayList<Integer> digits = new ArrayList<Integer>();
+        for(int d = 1; d<l+1; d++){
+            digits.add(d);
+        }
+        
+        return iArrList.containsAll(digits);
+    }
+
+    public static boolean isPrime(int num){
+        boolean flag = false;
+        for (int i = 2; i <= num / 2; ++i) {
+          // condition for nonprime number
+          if (num % i == 0) {
+            flag = true;
+            break;
+          }
+        }
+        return !flag;
+    }
+  
+    
+    public static void main(String[] args) {
+        
+        int pandigitalPrimeMax = 0;
+        
+        for(int number = 100000000; number>2; number--){
+            if(allDigits(number) && isPrime(number)){
+                pandigitalPrimeMax = number;
+                break;
+            }
+        }
+        
+        System.out.print(pandigitalPrimeMax + " is the largest pandigital prime.");
+    }
+}
