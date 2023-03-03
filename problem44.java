c9f621cd85e67bcc57928d6bd9f5788062f4653e
Lewis Wilson <55784291+lewisjwilson@users.noreply.github.com>
2023-03-03 09:45:14 +0900

    Create problem44.java

---
+++ problem44.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.math.*;
+
+class Pentagon {
+    
+    public static int pentagonal(int n){
+        return (int)(n*(3*n-1))/2;
+    }
+    
+    public static void main(String[] args) {
+        
+        List<Integer> pentagonals = new ArrayList<Integer>();
+        
+        for(int n = 1 ; n<=3000 ; n++){
+            pentagonals.add(pentagonal(n));
+        }
+        //System.out.println("Pentagonals: " + pentagonals.toString());
+        
+        int savedDiff = 0;
+        
+        boolean flag = false;
+        
+        for(Integer pj : pentagonals){
+            for(Integer pk : pentagonals){
+                if(pj != pk){
+                    int pSum = pj + pk;
+                    int pDiff = pk - pj;
+                    if(pentagonals.contains(pSum) && pentagonals.contains(pDiff)){
+                        flag = true;
+                        savedDiff = pDiff;
+                        System.out.print(pj + " is the " + pentagonals.indexOf(pj) + "th pentagonal number" + "\n");
+                        System.out.print(pk + " is the " + pentagonals.indexOf(pk) + "th pentagonal number" + "\n");
+                        System.out.print(pk + " + " + pj + " = " + pSum + ", the " + pentagonals.indexOf(pSum) + "th pentagonal number, and\n");
+                        System.out.print(pk + " - " + pj + " = " + pDiff + ", the " + pentagonals.indexOf(pDiff) + "th pentagonal number."); 
+                        break;
+                    }
+                }
+            }
+            if(flag){break;}
+        }
+        
+    }
+}
